4c9d116233f2be904c21ff82b2e71c97120bf41b
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-09-01 00:35:17 -0700

    start on premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,81 @@
+workspace "Volante"
+  configurations { "Debug", "Release" }
+  platforms { "Any CPU" }
+  flags { "Symbols" }
+
+  filter "action:vs*"
+    location "vs2015"
+  filter "action:monodevelop"
+    location "monodevelop"
+    defines { "MONO" }
+  filter {}
+
+  startproject "Tests"
+
+  project "Volante"
+    kind "SharedLib"
+    language "C#"
+    files { "src/*.cs", "src/impl/*.cs" }
+
+  project "Tests"
+    kind "ConsoleApp"
+    language "C#"
+    files { "tests/Tests/*.cs" }
+    links { "System" }
+    dependson { "Volante" }
+
+  project "DirectoryScan"
+    kind "ConsoleApp"
+    language "C#"
+    files { "examples/DirectoryScan/*.cs", "examples/DirectoryScan/Properties/*.cs" }
+    links { "System" }
+    dependson { "Volante" }
+
+  project "Guess"
+    kind "ConsoleApp"
+    language "C#"
+    files { "examples/Guess/*.cs" }
+    links { "System" }
+    dependson { "Volante" }
+
+  project "IpCountry"
+    kind "ConsoleApp"
+    language "C#"
+    files { "examples/IpCountry/*.cs" }
+    links { "System" }
+    dependson { "Volante" }
+
+  project "PropGuess"
+    kind "ConsoleApp"
+    language "C#"
+    files { "examples/PropGuess/*.cs" }
+    links { "System" }
+    dependson { "Volante" }
+
+  project "TestLink"
+    kind "ConsoleApp"
+    language "C#"
+    files { "examples/TestLink/*.cs" }
+    links { "System" }
+    dependson { "Volante" }
+
+  project "TestSOD"
+    kind "ConsoleApp"
+    language "C#"
+    files { "examples/TestSOD/*.cs" }
+    links { "System" }
+    dependson { "Volante" }
+
+  project "TestSSD"
+    kind "ConsoleApp"
+    language "C#"
+    files { "examples/TestSSD/*.cs" }
+    links { "System" }
+    dependson { "Volante" }
+
+  project "TransparentGuess"
+    kind "ConsoleApp"
+    language "C#"
+    files { "examples/TransparentGuess/*.cs" }
+    links { "System" }
+    dependson { "Volante" }
